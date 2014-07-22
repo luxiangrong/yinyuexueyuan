a6ac92205930e999d6d93b75aaa14a32261e4aa4
luxiangrong <luxiangrongwave@gmail.com>
2014-07-22 22:02:27 +0800

    基本完成，还剩下视差效果

--- script/function.js
+++ script/function.js
@@ -1,62 +1,71 @@
 (function($) {
 	// USE STRICT
 	"use strict";
-	
+
+	//实现学术活动处的动态效果
 	var sudden = {
-		init : function(){
-			if(!$(':has(.scholarship-wrap)')) {
+		init : function() {
+			if (!$(':has(.scholarship-wrap)')) {
 				return false;
 			}
 			var $suddenContainer = $(".scholarship-wrap");
-			var itemCount = $suddenContainer.find('.scholarship-item').size();
-			
-			var originItemsWidth = 0;
-			$suddenContainer.find('.scholarship-item').each(function(i,item){
-				originItemsWidth += $(item).width();
-			});
-			var rate = $window.width() / originItemsWidth;
-			$suddenContainer.find('.scholarship-item').each(function(i,item){
-				var originItemWidth = $(item).width();
-				var originItemHeight = $(item).height();
-				$(item).width(originItemWidth * rate);
-				$(item).find('img').width(originItemWidth * (rate + 0.1));
-				$(item).height(originItemHeight * rate);
-			});
-			
-			$suddenContainer.find('.scholarship-item').on('mouseenter', function(){
-				$suddenContainer.find('.scholarship-item').removeClass('selected-item');
-				$(this).data('origin-width', $(this).width());
-				$(this).addClass('selected-item');
-				
-				
-				var $addedWidth = $(this).find('img').width() - $(this).width();
-				
-				console.log($addedWidth);
-				
-				$suddenContainer.find('.scholarship-item').each(function(i, item){
-					$(this).width($(this).width() - $addedWidth / 4);
+
+			$suddenContainer.imagesLoaded(function() {
+				var itemCount = $suddenContainer.find('.scholarship-item').size();
+
+				var originItemsWidth = 0;
+				$suddenContainer.find('.scholarship-item').each(function(i, item) {
+					originItemsWidth += $(item).width();
 				});
-				
-				$(this).width($(this).find('img').width());
+				var rate = $window.width() / originItemsWidth;
+				$suddenContainer.find('.scholarship-item').each(function(i, item) {
+					var originItemWidth = $(item).width();
+					var originItemHeight = $(item).height();
+					$(item).width(originItemWidth * rate);
+					$(item).find('img').width(originItemWidth * (rate + 0.3));
+					$(item).height(originItemHeight * rate);
+				});
+
+				$suddenContainer.find('.scholarship-item').each(function(i, item) {
+					$(this).data('origin-width', $(this).width());
+				});
+
+				$suddenContainer.find('.scholarship-item').on('mouseenter', function() {
+					$suddenContainer.find('.scholarship-item').removeClass('selected-item');
+					$(this).addClass('selected-item');
+
+					var $addedWidth = $(this).find('img').width() - $(this).data('origin-width');
+
+					$suddenContainer.find('.scholarship-item').each(function(i, item) {
+						$(this).width($(this).data('origin-width') - $addedWidth / 4);
+					});
+
+					$(this).width($(this).find('img').width());
+				});
+				$suddenContainer.find('.scholarship-item').on('mouseleave', function() {
+					$(this).removeClass('selected-item');
+					$suddenContainer.find('.scholarship-item').each(function(i, item) {
+						if ($(this).data('origin-width') != undefined) {
+							$(this).width($(this).data('origin-width'));
+						}
+					});
+
+				});
 			});
-			$suddenContainer.find('.scholarship-item').on('mouseleave', function(){
-				if($(this).data('origin-width') != undefined) {
-					$(this).width($(this).data('origin-width'));
-				}
-			});
+
 		}
 	};
-	
+
 	var $window = $(window);
-	
+
 	var onReady = {
-		init: function(){
+		init : function() {
 			sudden.init();
 		}
 	};
-	
+
 	$(document).ready(onReady.init);
-	
+
 	$(function() {
 		$(document).ready(function() {
 			$(window).smoothScroll();
@@ -87,7 +96,6 @@
 				navigationText : ['&nbsp;', '&nbsp;'],
 				items : 4
 			});
-			
 
 		});
 	});
